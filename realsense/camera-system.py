151e133a06c4c4a0188f9c58d25f4dcd7a607148
Jimmy <jperez81@itam.mx>
2025-02-04 11:13:50 -0600

    just ordered stuff around, this commit doesnt really have any value other than to please my ocd

--- realsense/camera-system.py
+++ realsense/camera-system.py
@@ -1,9 +1,9 @@
 import cv2 as cv
 from ultralytics import YOLO
 from ultralytics.utils.plotting import Annotator, colors
-from depthcamera import get_pipe, get_streams
+from depthcamera import get_depth_stream_model
 
-def create_mask(results, annotator):
+def __create_mask(results, annotator) -> None:
     if results[0].boxes.id is not None and results[0].masks is not None:
         masks = results[0].masks.xy
         track_ids = results[0].boxes.id.int().cpu().tolist()
@@ -13,44 +13,35 @@ def create_mask(results, annotator):
             txt_color = annotator.get_txt_color(color)
             annotator.seg_bbox(mask=mask, mask_color=color, label=str(track_id), txt_color=txt_color)
 
-def main() -> None:
+
+def get_cam_stream_model(model_name: str) -> None:
     cam = cv.VideoCapture(0) # cv.VideoCapture(1, cv.CAP_DSHOW) in windows
     cam.set(cv.CAP_PROP_FRAME_WIDTH, 640)
-    cam.set(cv.CAP_PROP_FRAME_HEIGHT, 480)    
-    
-    pipe, align = get_pipe()
-    model = YOLO('realsense/yolo/yolo11x-seg.pt')
+    cam.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
+
+    model = YOLO(model_name)
     model.to('cuda')
     while True:
-        
-        """ret, cam_frame = cam.read()
+        ret, cam_frame = cam.read()
         if not ret:
-            break"""
+            break
 
-        color_image, depth_image = get_streams(camera_pipe=pipe, aligned=align)
-        depth_map = cv.applyColorMap(cv.convertScaleAbs(depth_image, alpha=0.5), cv.COLORMAP_JET)
-        
-        color_annotator = Annotator(color_image, line_width=2)
-        depth_annotator = Annotator(depth_map, line_width=2)
-        #cam_annotator = Annotator(cam_frame, line_width=2)
-        
-        realsense_results = model.track(color_image, persist=True)
-        #cam_results = model.track(cam_frame, persist=True)
-
-        create_mask(realsense_results, color_annotator)
-        create_mask(realsense_results, depth_annotator)
-        #create_mask(cam_results, cam_annotator)
-
-        cv.imshow("depth", depth_map)
-        cv.imshow("rgb", color_image)
-        #cv.imshow("test", cam_frame)
+        cam_annotator = Annotator(cam_frame, line_width=2)
+        cam_results = model.track(cam_frame, stream=True, persist=True)
+        __create_mask(cam_results, cam_annotator)
+        cv.imshow("cam", cam_frame)
 
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
 
     cam.release()
     cv.destroyAllWindows()
-    pipe.stop()
 
 
-main()
+def main() -> None:
+    get_depth_stream_model()
+    get_cam_stream_model()
+
+
+if __name__ == '__main__':
+    main()
--- realsense/depthcamera.py
+++ realsense/depthcamera.py
@@ -1,7 +1,21 @@
 import pyrealsense2 as rs
 import numpy as np
+import cv2 as cv
+from ultralytics import YOLO
+from ultralytics.utils.plotting import Annotator, colors
 
-def get_pipe() -> tuple:
+def __create_mask(results, annotator) -> None:
+    if results[0].boxes.id is not None and results[0].masks is not None:
+        masks = results[0].masks.xy
+        track_ids = results[0].boxes.id.int().cpu().tolist()
+
+        for mask, track_id in zip(masks, track_ids):
+            color = colors(int(track_id), True)
+            txt_color = annotator.get_txt_color(color)
+            annotator.seg_bbox(mask=mask, mask_color=color, label=str(track_id), txt_color=txt_color)
+
+
+def __get_pipe() -> tuple:
     pipe = rs.pipeline()
     config = rs.config()
 
@@ -15,7 +29,7 @@ def get_pipe() -> tuple:
     return pipe, align
 
 
-def get_streams(camera_pipe: object, aligned) -> tuple:
+def __get_streams(camera_pipe: object, aligned) -> tuple:
     frame = camera_pipe.wait_for_frames()
     aligned_frames = aligned.process(frame)
 
@@ -26,3 +40,28 @@ def get_streams(camera_pipe: object, aligned) -> tuple:
     color_image = np.asanyarray(color_frame.get_data())
 
     return color_image, depth_image
+
+
+def get_depth_stream_model(model_name: str) -> None:
+    pipe, align = __get_pipe()
+    model = YOLO(model_name)
+    model.to('cuda')
+
+    while True:
+        color_image, depth_image = __get_streams(pipe, align)
+        depth_map = cv.applyColorMap(cv.convertScaleAbs(depth_image, alpha=0.5), cv.COLORMAP_JET)
+
+        color_annotator = Annotator(color_image, line_width=2)
+        depth_annotator = Annotator(depth_map, line_width=2)
+
+        realsense_results = model.track(color_image, stream=True, persist=True)
+        __create_mask(realsense_results, color_annotator)
+        __create_mask(realsense_results, depth_annotator)
+
+        cv.imshow("rgb", color_image)
+        cv.imshow("depth", depth_map)
+
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    pipe.stop()
